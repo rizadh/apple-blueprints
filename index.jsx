19814a2f78ae2b1f458311e0ef2270d28711952c
Fatheen Nizam <fatheen.nizam@icloud.com>
2020-05-17 01:44:05 -0400

    Add year label

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ function MonthCard(props) {
     {props.products.map(product =>
       <li key={product.name}>
         <a href='#' className={product.isReleased ? 'released-product' : ''}>
-          <i className={product.isReleased ? 'fas fa-check-circle' : 'far fa-question-circle'}></i> {product.name}
+          <i className={product.isReleased ? 'fas fa-check-circle' : 'far fa-question-circle'} /> {product.name}
         </a>
       </li>
     )}
@@ -13,62 +13,79 @@ function MonthCard(props) {
   )
 }
 
-const data = [
+function YearCard(props) {
+  const totalProducts = props.months.reduce((total, currentMonth) => total + currentMonth.products.length, 0)
+
+  return (
+    <>
+      <div className="year">{props.year} ({totalProducts})</div>
+      <div className="container">
+        {props.months.map(month => <MonthCard key={month.name} month={month.name} products={month.products}/>)}
+      </div>
+    </>
+  )
+}
+
+const years = [
   {
-    month: 'March',
-    products: [
-      { name: 'iPad Pro', isReleased: true },
-      { name: 'Magic Keyboard', isReleased: true },
-      { name: 'MacBook Air' , isReleased: true },
-      { name: 'Mac mini', isReleased: true },
-      { name: 'Powerbeats', isReleased: true },
-    ],
-  },
-  {
-    month: 'April',
-    products: [
-      { name: 'iPhone SE', isReleased: true },
-    ],
-  },
-  {
-    month: 'May',
-    products: [
-      { name: '13" MacBook Pro' },
-      { name: 'iMac' },
-      { name: 'Apple TV' },
-    ],
-  },
-  {
-    month: 'June',
-    products: [
-      { name: 'AirTags' },
-      { name: 'AirPods Studio' },
-      { name: 'Wireless Charging Pad' },
-    ],
-  },
-  {
-    month: 'September',
-    products: [
-      { name: 'iPhone 12 (5.4")' },
-      { name: 'iPhone 12 (6.1")' },
-      { name: 'iPhone 12 Pro (6.1")' },
-      { name: 'iPhone 12 Pro (6.7")' },
-      { name: 'Apple Watch Series 6' },
-      { name: 'AirPods X' },
-      { name: 'HomePod' },
-      { name: 'HomePod Lite' },
-    ],
-  },
-  {
-    month: 'October',
-    products: [
-      { name: 'iPad Air' },
-      { name: 'iPad' },
+    yearName: '2020',
+    months: [
+      {
+        name: 'March',
+        products: [
+          { name: 'iPad Pro', isReleased: true },
+          { name: 'Magic Keyboard', isReleased: true },
+          { name: 'MacBook Air' , isReleased: true },
+          { name: 'Mac mini', isReleased: true },
+          { name: 'Powerbeats', isReleased: true },
+        ],
+      },
+      {
+        name: 'April',
+        products: [
+          { name: 'iPhone SE', isReleased: true },
+        ],
+      },
+      {
+        name: 'May',
+        products: [
+          { name: '13" MacBook Pro' },
+          { name: 'iMac' },
+          { name: 'Apple TV' },
+        ],
+      },
+      {
+        name: 'June',
+        products: [
+          { name: 'AirTags' },
+          { name: 'AirPods Studio' },
+          { name: 'Wireless Charging Pad' },
+        ],
+      },
+      {
+        name: 'September',
+        products: [
+          { name: 'iPhone 12 (5.4")' },
+          { name: 'iPhone 12 (6.1")' },
+          { name: 'iPhone 12 Pro (6.1")' },
+          { name: 'iPhone 12 Pro (6.7")' },
+          { name: 'Apple Watch Series 6' },
+          { name: 'AirPods X' },
+          { name: 'HomePod' },
+          { name: 'HomePod Lite' },
+        ],
+      },
+      {
+        name: 'October',
+        products: [
+          { name: 'iPad Air' },
+          { name: 'iPad' },
+        ],
+      },
     ],
   },
 ]
 
-const container = document.querySelector('.container')
-// PROPS                                       ->
-const monthCards = data.map(item => <MonthCard key={item.month} month={item.month} products={item.products}/>)
-ReactDOM.render(monthCards, container)
+// PROPS                                     ->
+const yearCards = years.map(year => <YearCard key={year.yearName} year={year.yearName} months={year.months}/>)
+ReactDOM.render(yearCards, document.querySelector('.wrapper'))
