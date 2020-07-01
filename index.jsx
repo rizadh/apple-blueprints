0033a6b455c3425969f74cd837f13cbb66e122e1
Fatheen Nizam <fatheen.nizam@icloud.com>
2020-06-30 22:53:52 -0400

    Remove overlay blur

--- index.jsx
+++ index.jsx
@@ -1,16 +1,21 @@
-import years from './data'
+import years from "./data";
 
 function ProductItem({ product }) {
-  const { name, status } = product
-  const [showDetails, setShowDetails] = React.useState(false)
-  const showModal = React.useCallback(() => setShowDetails(true))
-  const closeModal = React.useCallback(() => setShowDetails(false))
+  const { name, status } = product;
+  const [showDetails, setShowDetails] = React.useState(false);
+  const showModal = React.useCallback(() => setShowDetails(true));
+  const closeModal = React.useCallback(() => setShowDetails(false));
 
   return (
     <>
       <li>
-        <a href='#' className={status + '-product'} onClick={showModal}>
-          <i className={status ? 'fas fa-check-circle' : 'far fa-question-circle'} /> {name}
+        <a href="#" className={status + "-product"} onClick={showModal}>
+          <i
+            className={
+              status ? "fas fa-check-circle" : "far fa-question-circle"
+            }
+          />{" "}
+          {name}
         </a>
       </li>
       {showDetails && (
@@ -20,16 +25,18 @@ function ProductItem({ product }) {
         </Modal>
       )}
     </>
-  )
+  );
 }
 
 function MonthCard({ month, products, status }) {
   return (
     <ul className="item">
       {month} <span className="counter">{products.length}</span>
-      {products.map(product => <ProductItem key={product.name} product={product} />)}
+      {products.map((product) => (
+        <ProductItem key={product.name} product={product} />
+      ))}
     </ul>
-  )
+  );
 }
 
 function YearCard({ months, year }) {
@@ -37,60 +44,89 @@ function YearCard({ months, year }) {
     <>
       <div className="year">{year}</div>
       <div className="container">
-        {months.map(({ name, products }) => <MonthCard key={name} month={name} products={products} />)}
+        {months.map(({ name, products }) => (
+          <MonthCard key={name} month={name} products={products} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-function ProductContainer({ product: { name, status, description, features, sources }, onDismiss }) {
+function ProductContainer({
+  product: { name, status, description, features, sources },
+  onDismiss,
+}) {
   return (
     <div className="product-container">
-
-      <i className={status ? 'fas fa-check-circle product-status released-product' : 'far fa-question-circle product-status rumoured-product'} />
-      <div className={status ? 'product-status released-product' : 'product-status rumoured-product'}>&nbsp;{status ? 'Released' : 'Rumoured'}</div>
+      <i
+        className={
+          status
+            ? "fas fa-check-circle product-status released-product"
+            : "far fa-question-circle product-status rumoured-product"
+        }
+      />
+      <div
+        className={
+          status
+            ? "product-status released-product"
+            : "product-status rumoured-product"
+        }
+      >
+        &nbsp;{status ? "Released" : "Rumoured"}
+      </div>
 
       <div className="product-name">{name}</div>
       <div className="product-description">{description}</div>
 
-      {features &&
+      {features && (
         <div className="product-header">
           Features
           <ul className="product-features">
-            {features.map(feature => <li key={feature}>{feature}</li>)}
+            {features.map((feature) => (
+              <li key={feature}>{feature}</li>
+            ))}
           </ul>
         </div>
-      }
-      {sources &&
+      )}
+      {sources && (
         <div className="product-header">
           Sources
           <ul className="product-features">
-            {sources.map(source => <li key={source} className="source-link"><a href={source.link} target="_blank" className="source-link">{source.name}</a></li>)}
+            {sources.map((source) => (
+              <li key={source} className="source-link">
+                <a href={source.link} target="_blank" className="source-link">
+                  {source.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
-      }
+      )}
 
-      <div className="okay-button" onClick={onDismiss}>Okay</div>
-
+      <div className="okay-button" onClick={onDismiss}>
+        Okay
+      </div>
     </div>
-  )
+  );
 }
 
 function Modal({ children }) {
-  const element = React.useRef(document.createElement('div'))
+  const element = React.useRef(document.createElement("div"));
 
   React.useEffect(() => {
-    const parentNode = document.querySelector('#modal-container')
-    parentNode.appendChild(element.current)
+    const parentNode = document.querySelector("#modal-container");
+    parentNode.appendChild(element.current);
 
-    return () => element.current.remove()
-  })
+    return () => element.current.remove();
+  });
 
-  return ReactDOM.createPortal(children, element.current)
+  return ReactDOM.createPortal(children, element.current);
 }
 
 function App() {
-  return years.map(({ yearName, months }) => <YearCard key={yearName} year={yearName} months={months} />)
+  return years.map(({ yearName, months }) => (
+    <YearCard key={yearName} year={yearName} months={months} />
+  ));
 }
 
-ReactDOM.render(<App />, document.querySelector('.wrapper'))
+ReactDOM.render(<App />, document.querySelector(".wrapper"));
