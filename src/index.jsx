ccd8ea0e2e70f2b4cb20da1d6f653f6364da19b7
Rizadh Nizam <me@rizadh.com>
2024-07-01 16:06:27 -0400

    Migrate to Vite

--- src/index.jsx
+++ src/index.jsx
@@ -212,8 +212,8 @@ function App() {
 }
 
 async function fetchData() {
-  const spaceId = process.env.CONTENTFUL_SPACE_ID;
-  const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+  const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
+  const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
   const apiUrl = `https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}&content_type=product`;
   const data = await fetch(apiUrl).then((res) => res.json());
 
