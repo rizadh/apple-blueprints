a440c7f8415da3398769676c4827482dc22d2822
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-07-01 18:08:17 -0400

    Migrate to Vite

--- src/index.jsx
+++ src/index.jsx
@@ -103,8 +103,8 @@ function LastUpdated({ lastUpdated }) {
 }
 
 async function fetchProductsData() {
-  const spaceId = process.env.CONTENTFUL_SPACE_ID;
-  const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+  const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
+  const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
   const apiUrl = `https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}&content_type=product`;
   const data = await fetch(apiUrl).then((res) => res.json());
 
