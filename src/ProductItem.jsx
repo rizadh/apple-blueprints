0c807cb2a67d15f946a93eba0e26528db48595d1
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-07-02 00:15:35 -0400

    Add underline to product items

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { statusIcons } from "./statusIcons";
 import { useContentfulLiveUpdates } from "@contentful/live-preview/react";
@@ -6,17 +6,23 @@ import { ContentfulLivePreview } from "@contentful/live-preview";
 
 export function ProductItem({ product }) {
   const updatedProduct = useContentfulLiveUpdates(product);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <Link
       to={"/product/" + updatedProduct.fields.slug}
       className={updatedProduct.fields.status + "-product product-item"}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <i
         className={`${statusIcons[updatedProduct.fields.status]} product-item-icon`}
         {...ContentfulLivePreview.getProps({ entryId: product.sys.id, fieldId: "status" })}
       />{" "}
-      <span {...ContentfulLivePreview.getProps({ entryId: product.sys.id, fieldId: "productName" })}>
+      <span
+        className={`product-item-text ${isHovered ? "hovered-text" : ""}`}
+        {...ContentfulLivePreview.getProps({ entryId: product.sys.id, fieldId: "productName" })}
+      >
         {updatedProduct.fields.productName}
       </span>
     </Link>
